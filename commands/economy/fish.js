d6dfcac59efd451310f6eb063eac5a47648a0c9f
Milad Mehri <memm.milad@gmail.com>
2021-04-28 19:18:54 -0700

    Update fish.js

--- commands/economy/fish.js
+++ commands/economy/fish.js
@@ -6,25 +6,12 @@ module.exports = {
 	name: 'fish',
 	description: 'Use your fishing rod to find fish!',
 	usage: '`a.fish`',
-  category: 'economy',
+	category: 'economy',
+	cooldown: 10,
 	async execute(message) {
 
 		var result = await db.fetch(message.author.id)
 
-		let cooldown = 5 * 1000;; // 12 hours in ms
-
-		let lastfish = parseInt(result.fishcd + '000')
-
-		if (lastfish !== null && cooldown - (Date.now() - lastfish) > 1) {
-			// If user still has a cooldown
-			let timeObj = cooldown - (Date.now() - lastfish);
-			return embeds.cooldownEmbed(message, timeObj);
-		} else {
-			var time = Date.now().toString().slice(0, -3)
-			await db.set(message.author.id, 'fishcd', time)
-			
-
-		}
 
 
 
@@ -34,7 +21,7 @@ module.exports = {
 		if (random < 11) {
 			await db.set(message.author.id, 'rod', result.rod - 1)
 			message.channel.send(emojis.fishing + ' fishing...').then(msg => {
-				setTimeout(function() {
+				setTimeout(function () {
 
 					return msg.edit(':fishing_pole_and_fish: You went fishing and **hit a rock**\nNow you lost your rod :sob: ')
 
@@ -42,15 +29,15 @@ module.exports = {
 			})
 
 		} else if (random > 10 && random < 41) {
-			message.channel.send(emojis.fishing +  ' fishing...').then(msg => {
-				setTimeout(function() {
+			message.channel.send(emojis.fishing + ' fishing...').then(msg => {
+				setTimeout(function () {
 
 					return msg.edit(':fishing_pole_and_fish: You went fishing and came back with nothing')
 				}, 2000);
 			})
 		} else if (random > 40 && random < 80) {
 			message.channel.send(emojis.fishing + ' fishing...').then(msg => {
-				setTimeout(function() {
+				setTimeout(function () {
 
 					db.set(message.author.id, 'common', result.common + common)
 					return msg.edit(':fishing_pole_and_fish: You went fishing and came back with **' + common + ' common fish **:fish: ')
@@ -58,7 +45,7 @@ module.exports = {
 			})
 		} else {
 			message.channel.send(emojis.fishing + ' fishing...').then(msg => {
-				setTimeout(function() {
+				setTimeout(function () {
 
 					db.set(message.author.id, 'rare', result.rare + 1)
 					return msg.edit(':fishing_pole_and_fish: You went fishing and came back with **' + 1 + ' rare fish **:tropical_fish:  ')
