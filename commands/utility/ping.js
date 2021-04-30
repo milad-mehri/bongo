a555839e22563986c7f77e28eb148deab3fce16e
Milad Mehri <memm.milad@gmail.com>
2021-04-30 16:26:10 -0700

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -5,7 +5,7 @@ module.exports = {
 	name: 'ping',
 	description: 'Get the bots ping',
 	usage: '`a.ping`',
-  category: 'utility',
+	category: 'utility',
 
 	async execute(message, args) {
 		message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(message.client.ws.ping)}ms`);
