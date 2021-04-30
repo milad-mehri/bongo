6d3b97e75fbab9f2242576298e69e923f6983cc9
Milad Mehri <memm.milad@gmail.com>
2021-04-30 15:44:02 -0700

    Update give.js

--- commands/economy/give.js
+++ commands/economy/give.js
@@ -20,111 +20,34 @@ module.exports = {
 
 	async execute(message, args) {
 
+		var amount;
+
+
+		if (!message.mentions.users.first()) return embeds.errorEmbed(message, 'You have to **mention** someone!')
+		if (args[0][0] === '<') {
+			amount = parseInt(args[1])
+		}else{
+			amount = parseInt(args[0])
+		}
+
+		if(!parseInt(amount)) return embeds.errorEmbed(message, 'You have to say **how much** you want to give.')
+
+
 		var giver = message.author;
 		var reciever = message.mentions.users.first();
-		if (giver === reciever) {
-			return embeds.errorEmbed(message, 'You **can\'t send money to yourself**!:angry: ');
-		}
-		var a = args[1]
-		var b = args[0]
+		if(giver.id === reciever.id) return message.errorEmbed(message, 'You can\'t send money to your self!')
+		var recieverResults = await db.fetch(reciever.id)
 
-		var result = await db.fetch(giver.id)
+		var giverResults = await db.fetch(giver.id)
+		if(giverResults.bal < amount) return message.errorEmbed(message, "You don't have enough money!")
+		if(amount < 1) return message.errorEmbed(message, "What money are you trying to give? lmao.")
 
-		var giverbal = result.bal
-		if (giverbal < 1) return embeds.errorEmbed(message, '**What money** are you giving away?** lmao**.')
+		await db.set(giver.id, "bal", giverResults.bal - amount)
+		await db.set(reciever.id, "bal", recieverResults.bal + amount)
 
-
-		if (reciever === undefined) {
-			return embeds.errorEmbed(message, ':angry: **mention** who you want to **give money** to.');
-		}
-
-
-		var result = await db.fetch(message.author.id)
-
-		var bal = result.bal
-
-
-		if (a === 'max' || a === 'all') {
-			a = bal
-		}
-		if (b === 'max' || b === 'all') {
-			b = bal
-		}
-
-
-		if (a === 'half') {
-			a = parseInt(bal) / 2
-		}
-		if (b === 'half') {
-			b = parseInt(bal) / 2
-		}
-
-
-
-
-		a = parseInt(a)
-
-		b = parseInt(b)
-
-
-
-
-		if (isNaN(parseInt(a))) {
-			if (isNaN(parseInt(b))) {
-				embeds.errorEmbed(message, ':angry: **Invalid input**... ');
-			} else {
-
-				if (parseInt(giverbal) >= parseInt(b)) {
-					var bal = result.bal
-					if (parseInt(b) < 0) {
-						return embeds.errorEmbed(message, 'You can\'t send negative money')
-					}
-
-
-					var result = await db.set(giver.id, 'bal', parseInt(bal) - parseInt(b))
-					var sendbal = parseInt(bal) - parseInt(b)
-					result = await db.fetch(reciever.id)
-					var bal = result.bal
-
-					await db.set(reciever.id, 'bal', parseInt(bal) + parseInt(b))
-
-					message.channel.send(
-						`You gave **${reciever.username} **${functions.comma(b)}** coins. Now you have **$${functions.comma(sendbal)}** and they have **$${functions.comma(bal + parseInt(b))}**.`
-					);
-				} else {
-					embeds.errorEmbed(message, 'You dont have enough money!');
-				}
-			}
-		} else {
-			if (parseInt(a) < 0) {
-				return embeds.errorEmbed(message, 'You **can\'t** send **negative money**!')
-			}
-			var result = await db.fetch(giver.id)
-			var giverbal = result.bal
-			if (giverbal >= a) {
-				var bal = result.bal
-
-				await db.set(giver.id, 'bal', parseInt(bal) - parseInt(a))
-				var sendbal = parseInt(bal) - parseInt(a)
-				var result = await db.fetch(reciever.id)
-				var bal = result.bal
-				await db.set(reciever.id, 'bal', parseInt(bal) + parseInt(a))
-				return message.channel.send(
-					`You gave **${reciever.username} **${functions.comma(b)}** coins. Now you have **$${functions.comma(sendbal)}** and they have **$${functions.comma(bal + parseInt(b))}**.`
-				);
-			} else {
-				embeds.errorEmbed(message,'You dont have enough money!');
-			}
-		}
-
-
-
-
-
-
-
-
-
+		message.reply(
+			`You gave ${reciever.username} **$${functions.comma(amount)}**, now you have **$${giverResults.bal - amount}** and they have **$${recieverResults.bal + amount}**.`
+		)
 
 
 	},
