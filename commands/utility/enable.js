5028618c36a475da52aaab50c454fcb04783f849
Milad Mehri <memm.milad@gmail.com>
2021-04-30 17:07:16 -0700

    Create enable.js

---
+++ commands/utility/enable.js
@@ -0,0 +1,38 @@
+
+const Discord = require('discord.js');
+const db = require("../../db")
+const embeds = require("../../functions/embeds")
+
+module.exports = {
+    name: 'enable',
+    description: 'enable commands!',
+    usage: '`a.enable <command>`',
+    category: 'utility',
+
+    async execute(message, args) {
+
+
+        var command = message.client.commands.get(args[0]) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(args[0]));
+        if(["enable", "enable"].includes(command.name)) return embeds.errorEmbed(message, "This command cannot be enabled!")
+
+        if (!command) return message.channel.send("This is not a valid command.")
+
+        if (!message.member.hasPermission('ADMIN')) {
+            return message.reply('You cannot use the **enable** command.');
+        }
+
+
+        var result = await db.fetchguild(message.guild.id)
+        if (!result.enabled[command.name]) return message.channel.send("This command is already enabled.")
+
+        
+        result.enabled[command.name] = false
+        await db.guildset(message.guild.id, "enabled", result.enabled)
+
+        console.log(result.enabled)
+        return embeds.successEmbed(message, `${command["name"]} was enabled!`)
+
+
+
+	},
+};
