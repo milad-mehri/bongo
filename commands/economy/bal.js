c61270a8b340e18720573d01469428002d09f69d
Milad Mehri <memm.milad@gmail.com>
2021-04-30 22:00:59 -0700

    Update bal.js

--- commands/economy/bal.js
+++ commands/economy/bal.js
@@ -29,35 +29,15 @@ module.exports = {
 				invworth += result[item.name] * item.price
 			})
 
-			embeds.defaultEmbed(
-				message,
-				`${message.author.username}'s Balance`,
-				`:bank:  Balance: [$${functions.comma(result.bal)}](https://top.gg/bot/780943575394942987)\n
-				:file_cabinet:  Inventory: [$${functions.comma(invworth)}](https://top.gg/bot/780943575394942987)\n
-				:globe_with_meridians:  Net worth (total): [$${functions.comma(result.bal + invworth)}](https://top.gg/bot/780943575394942987)`
-			);
-
-
-
+			embeds.defaultEmbed(message, `${message.author.username}'s Balance`, `:bank:  Balance: [$${functions.comma(result.bal)}](https://top.gg/bot/780943575394942987)\n\n:file_cabinet:  Inventory: [$${functions.comma(invworth)}](https://top.gg/bot/780943575394942987)\n\n:globe_with_meridians:  Net worth (total): [$${functions.comma(result.bal + invworth)}](https://top.gg/bot/780943575394942987)`);
 		} else {
 			var user = message.mentions.users.first();
 			const result = await db.fetch(user.id)
 			var invworth = 0
-
 			message.client.items.forEach(item => {
 				invworth += result[item.name] * item.price
 			})
-
-			embeds.defaultEmbed(
-				message,
-				`${user.username}'s Balance`,
-				`:bank:  Balance: [$${functions.comma(result.bal)}](https://top.gg/bot/780943575394942987)\n
-				:file_cabinet:  Inventory: [$${functions.comma(invworth)}](https://top.gg/bot/780943575394942987)\n
-				:globe_with_meridians:  Net worth (total): [$${functions.comma(result.bal + invworth)}](https://top.gg/bot/780943575394942987)`
-			);
-
-
-
+			embeds.defaultEmbed(message, `${user.username}'s Balance`, `:bank:  Balance: [$${functions.comma(result.bal)}](https://top.gg/bot/780943575394942987)\n\n:file_cabinet:  Inventory: [$${functions.comma(invworth)}](https://top.gg/bot/780943575394942987)\n\n:globe_with_meridians:  Net worth (total): [$${functions.comma(result.bal + invworth)}](https://top.gg/bot/780943575394942987)`);
 		}
 
 
