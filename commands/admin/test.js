f88a34a3706603490da75ba0c22e0d7563e87d9c
Milad Mehri <memm.milad@gmail.com>
2021-05-01 18:09:29 -0700

    schema update

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const embeds = require('../../functions/embeds.js');
-const db = require("../../db")
-
+const userSchema = require('../../schemas/user-schema')
+const db = require('../../db')
 module.exports = {
     name: 'test',
     description: 'Admin command',
@@ -13,13 +13,43 @@ module.exports = {
         }
 
 
-        var result = await db.fetch (message.author.id)
-        result.businessObject.stock = "ddddddddd"
 
 
-        await db.set(message.author.id, 'businessObject', result.businessObject)
+        var result = await db.fetch(message.author.id)
 
+        var newUser = {
+            "userid": result.userid,
+
+            "items": {
+                "ball": result.ball,
+                "box": result.box,
+                "clover": result.clover,
+                "common": result.common,
+                "diamond": result.diamond,
+                "donut": result.donut,
+                "medal": result.medal,
+                "rare": result.rare,
+                "rod": result.rod,
+                "shield": result.shield,
+                "bleach" : result.bleach
+            },
+            businessObject: {
+                "bal": result.busbal,
+                "stock": result.busstock,
+                "name": result.business
+
+            },
+            "bal": result.bal,
+            "win": result.win,
+            "loss": result.loss,
+            "autolottery": result.autolottery,
+            "enteredlottery": result.enteredlottery,
+        }
+
+        await userSchema.findOneAndDelete({ userid: message.author.id })
+        await new userSchema(newUser).save()
         console.log("DONE")
-
     },
 };
+
+//do you mind testing bongo beta
--- schemas/user-schema.js
+++ schemas/user-schema.js
@@ -15,12 +15,11 @@ const userSchema = mongoose.Schema({
 		required: false,
 	},
 	busbal: {
-		default: 0,
+		type:Number,
 	},
 	busbal: {
 		type: Number,
 		required: false,
-		default: 0,
 	},
 	bal: {
 		default: 1000,
@@ -29,64 +28,47 @@ const userSchema = mongoose.Schema({
 	},
 	ball: {
 		type: Number,
-		default: 0,
 	},
 	shield: {
 		type: Number,
-		default: 0,
 	},
 	medal: {
 		type: Number,
-		default: 0,
 	},
 	common: {
 		type: Number,
-		default: 0,
 	},
 	rare: {
 		type: Number,
-		default: 0,
 	},
 	diamond: {
 		type: Number,
-		default: 0,
 	},
 	bleach: {
 		type: Number,
-		default: 0,
 	},
 	clover: {
 		type: Number,
-		default: 0,
 	},
 	box: {
 		type: Number,
-		default: 1,
 
 	},
 	donut: {
 		type: Number,
-		default: 1,
 
 	},
 	rod: {
 		type: Number,
-		default: 0,
 
 	},
 	enteredlottery: {
 		type: Boolean,
 		default: false,
 	},
-	_autolottery: {
+	autolottery: {
 		type: Boolean,
 		default: false,
-	},
-	get autolottery() {
-		return this._autolottery
-	},
-	set autolottery(value) {
-		this._autolottery = value
 	},
 	banned: {
 		type: Boolean,
