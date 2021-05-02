b7cd5213e088183b96edfc6f58a0e58c08fc3721
Milad Mehri <memm.milad@gmail.com>
2021-05-02 12:47:07 -0700

    Update prune.js

--- commands/utility/prune.js
+++ commands/utility/prune.js
@@ -8,6 +8,8 @@ module.exports = {
   category: 'utility',
 
 	async execute(message, args) {
+
+		if(['max', 'all'].includes(args[0].toLowerCase())) args[0] = 99
 		const amount = parseInt(args[0]);
 
 		if (!message.member.hasPermission('MANAGE_MESSAGES')) {
@@ -16,7 +18,7 @@ module.exports = {
 
 		if (isNaN(amount)) {
 			return message.channel.send("that doesn't seem to be a valid number.");
-		} else if (amount < 1 || amount > 100) {
+		} else if (amount < 1 || amount > 99) {
 			return message.channel.send(
 				'You need to input a number between **1** and **99**.'
 			);
