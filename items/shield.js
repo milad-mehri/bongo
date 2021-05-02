9c679e383902f001f6d95da1e7975444a6aaaf00
Milad Mehri <memm.milad@gmail.com>
2021-05-01 23:12:13 -0700

    fix shield.js

--- items/shield.js
+++ items/shield.js
@@ -1,4 +1,7 @@
 const Discord = require('discord.js');
+const embeds = require('../functions/embeds')
+const functions = require('../functions/functions')
+const db = require("../db")
 
 module.exports = {
     name: 'shield',
@@ -10,7 +13,21 @@ module.exports = {
     description: "Protect yourself from any robbers!",
 
     async execute(message, args, result) {
-        if(!result.items.shield) return message.channel.send('You have no shields to use.')
-        message.channel.send('Shields are always being used.')
+        var result = await db.fetch(message.author.id)
+
+        if ((result.rob.invincibleStart + result.rob.invinciblityTime) > Date.now() && args[0].toLowerCase() !== "y") {
+            var time = functions.msToString((result.rob.invincibleStart + result.rob.invinciblityTime - Date.now()))
+            var sendMessage = "`a.use shield f`"
+            return message.channel.send(`You are already safe for **${time}**.\nBut you can do ${sendMessage} to use your shield!`)
+        }
+
+
+        result.rob.invinciblityTime = 43200 * 1000
+
+        result.rob.invincibleStart = Date.now()
+
+        await db.set(message.author.id, "rob", result.rob)
+        return embeds.successEmbed(message, "Shield was used, you are now safe for **12 hours**!")
+
     }
-} 
+}
