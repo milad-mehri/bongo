b6f89d3f4f50a70a3ed55e3a7921170b7001316a
Milad Mehri <memm.milad@gmail.com>
2021-05-09 09:41:03 -0700

    fix ball glitch

--- commands/economy/sell.js
+++ commands/economy/sell.js
@@ -34,28 +34,30 @@ module.exports = {
 
 
 		item = item.toLowerCase()
-		if(!amount) amount = 1
+		if (!amount) amount = 1
 
 		if (parseInt(amount) < 1) {
 			return embeds.errorEmbed(message, 'You have to sell something.')
 		}
 		item = (message.client.items.get(item) || message.client.items.find(cmd => cmd.aliases && cmd.aliases.includes(item)) || null)
 		if (!item) return embeds.blankEmbed(message, 'This item isnt in the shop!')
-		if (result.items[item.name] < amount) return embeds.errorEmbed(message,'You **don\'t have enough** of this item')
-
+		if (result.items[item.name] < amount) return embeds.errorEmbed(message, 'You **don\'t have enough** of this item')
 
 
 
 		let bal = result.bal
 		var price = item.price * amount
+		result.items[item.name] = result.items[item.name] - parseInt(amount)
 
 
-		await db.set(message.author.id, 'bal', bal + price * 0.10)
-
-		result.items[item] = result.items[item] - parseInt(amount)
+		console.log(item)
 
 		await db.set(message.author.id, 'items', result.items)
-		embeds.defaultEmbed(message ,'Package returned', `You sold **${functions.comma(amount)} ${item.displayName}'s** for **$${functions.comma(price * 0.10)}** and you now have **${functions.comma(parseInt(result.items[item.name]))} ${item.displayName}'s** and **$${functions.comma(bal + (price * 0.10))} coins**.`)
+		await db.set(message.author.id, 'bal', bal + price * 0.10)
+
+
+
+		embeds.defaultEmbed(message, 'Package returned', `You sold **${functions.comma(amount)} ${item.displayName}'s** for **$${functions.comma(price * 0.10)}** and you now have **${functions.comma(parseInt(result.items[item.name]))} ${item.displayName}'s** and **$${functions.comma(bal + (price * 0.10))} coins**.`)
 
 
 	},
